ff504885aaa6ea5cabb647c451a07a80b2477696
Straixes <quentinlang38@gmail.com>
2024-04-01 11:58:50 +0200

    Start
    Creation on the project, the main java class and the principalFrame java class

---
+++ Reminder-Manager-Java/src/fr/straixes/Main.java
@@ -0,0 +1,9 @@
+package fr.straixes;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        PrincipalFrame principalFrame = new PrincipalFrame();
+    }
+}
---
+++ Reminder-Manager-Java/src/fr/straixes/PrincipalFrame.java
@@ -0,0 +1,15 @@
+package fr.straixes;
+
+import javax.swing.*;
+
+public class PrincipalFrame extends JFrame {
+
+    PrincipalFrame() {
+
+        this.setTitle("Reminder-Manager");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(1000, 800);
+        this.setVisible(true);
+
+    }
+}
